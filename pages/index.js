23ee7243852d28702b294015ceeda70fcd109811
Levy-B <josephlevy.batario@gsfe.tupcavite.edu.ph>
2022-12-03 12:56:46 +0800

    created forms

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,9 @@ export default function Home() {
         <title>Create Account</title>
         <meta
           name="description"
+          
           content="Technological University of the Philippines - Cavite"
+          
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
